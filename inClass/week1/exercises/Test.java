5b7e409a0d8df1296bb239277bd4ca35ddc28cf9
Michael Aldridge <aldridge.mac@gmail.com>
2014-09-02 15:11:33 -0500

    complete in class assignment

---
+++ inClass/week1/exercises/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+	int x; // x has no default value
+	String y; // y has no default value
+	System.out.println("x is " + x); 
+	System.out.println("y is " + y); 
+    }
+}
