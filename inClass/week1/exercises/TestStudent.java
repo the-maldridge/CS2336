d60eca6941dcce0d30563c960b868d43bbd6021a
Michael Aldridge <aldridge.mac@gmail.com>
2014-09-02 15:09:00 -0500

    complete in class exercise

---
+++ inClass/week1/exercises/TestStudent.java
@@ -0,0 +1,9 @@
+public class TestStudent {
+    public static void main(String[] args) {
+	Student student = new Student();
+	System.out.println("name? " + student.name); 
+	System.out.println("age? " + student.age); 
+	System.out.println("isScienceMajor? " + student.isScienceMajor); 
+	System.out.println("gender? " + student.gender); 
+    }
+}
