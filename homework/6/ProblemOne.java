358f41691e7d7d021e857c30d07467d7d795aad1
Michael Aldridge <aldridge.mac@gmail.com>
2014-11-14 01:18:15 -0600

    finish problem one

--- homework/6/ProblemOne.java
+++ homework/6/ProblemOne.java
@@ -2,15 +2,15 @@ import java.util.UUID;
 
 public class ProblemOne {
     public static void main(String[] args) {
-	LinkedList myList = new LinkedList<Integer>();
+	LinkedList<Integer> myList = new LinkedList<Integer>();
 	for(int i=0; i<100; i++) {
-	    myList.add(new Node(i));
+	    myList.add(new Node<Integer>(i));
 	}
 	System.out.println("The size of the integer list is: " + myList.size());
     
-	LinkedList strList = new LinkedList<String>();
+	LinkedList<String> strList = new LinkedList<String>();
 	for(int i=0; i<25; i++) {
-	    myList.add(new Node(UUID.randomUUID().toString()));
+	    strList.add(new Node<String>(UUID.randomUUID().toString()));
 	}
 	System.out.println("The size of the string list is: " + strList.size());
     }
@@ -19,11 +19,7 @@ public class ProblemOne {
 
 //just enough of a type specialized LinkedList to host the size function
 class LinkedList<E> {
-    private Node<E> head;
-
-    public LinkedList() {
-	head = null;
-    }
+    private Node<E> head = null;
 
     public void add(Node<E> n) {
 	Node search = head;
@@ -52,7 +48,7 @@ class LinkedList<E> {
 
 class Node<E> {
     private E data;
-    private Node next;
+    private Node next=null;
 
     public Node(E data) {
 	this.data = data;
@@ -64,5 +60,9 @@ class Node<E> {
 
     public void setNext(Node next) {
 	this.next = next;
+    }
+
+    public String toString() {
+	return data.toString();
     }
 }
